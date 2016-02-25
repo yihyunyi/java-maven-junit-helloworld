000e43b4f45ae40dec16c529caf4d702eb3271b3
yihyunyi <yihyunyi@bluedigm.com>
2016-02-25 06:08:20 +0000

    fix pmd violation

--- src/main/java/com/example/javamavenjunithelloworld/HelloApp.java
+++ src/main/java/com/example/javamavenjunithelloworld/HelloApp.java
@@ -6,10 +6,10 @@ package com.example.javamavenjunithelloworld;
  */
 public class HelloApp {
 
-    static int DEFAULT_TIMES = 3;
+    static final int DEFAULT_TIMES = 3;
 
-    static int EXIT_STATUS_PARAMETER_NOT_UNDERSTOOD = 2;
-    static int EXIT_STATUS_HELLO_FAILED = 4;
+    static final int EXIT_STATUS_PARAMETER_NOT_UNDERSTOOD = 2;
+    static final int EXIT_STATUS_HELLO_FAILED = 4;
 
     /**
      * The main method of this program.
@@ -23,8 +23,8 @@ public class HelloApp {
             try {
                 times = Integer.valueOf(args[0]);
             } catch (NumberFormatException e) {
-                System.err.println("I don't understand the parameter you passed me. Is it a number? " +
-                        "Parameter was: [" + args[0] + "]");
+                //System.err.println("I don't understand the parameter you passed me. Is it a number? " +
+                //        "Parameter was: [" + args[0] + "]");
                 System.exit(EXIT_STATUS_PARAMETER_NOT_UNDERSTOOD);
             }
         }
@@ -33,7 +33,7 @@ public class HelloApp {
         try {
             hi.setTimes(times);
         } catch (IllegalArgumentException e) {
-            System.err.println("Something went wrong: " + e.getMessage());
+            //System.err.println("Something went wrong: " + e.getMessage());
             System.exit(EXIT_STATUS_HELLO_FAILED);
         }
         hi.sayHello(System.out);
